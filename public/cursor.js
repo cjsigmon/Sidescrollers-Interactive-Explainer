d8a12f2a45a73cf5cc2fd0f69cea2d3732dca323
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-10-03 15:01:07 -0400

    cursor fixes

--- public/cursor.js
+++ public/cursor.js
@@ -1,22 +1,28 @@
-$(document).on('mousemove', function(e){
-
-
-    if($('#start-btn:hover').length != 0) {
-        $('#custom-cursor').css({
-            left:  e.pageX,
-            top:   e.pageY,
-            "background-image": "url('img/cursor-pointer.png')",
-            pointerEvents: 'none'
-        });
-    } else {
-        $('#custom-cursor').css({
-            left:  e.pageX,
-            top:   e.pageY,
-            "background-image": "url('img/cursor-normal.png')",
-            pointerEvents: 'none'
-        });
-    }
+$(document).ready(function() {
     $('body').css({
         cursor: 'none'
     });
-});
+
+
+    $(document).on('mousemove', function(e){
+        if($('#start-btn:hover').length != 0) {
+            $('#custom-cursor').css({
+                left:  e.pageX,
+                top:   e.pageY,
+                "background-image": "url('img/cursor-pointer.png')",
+                pointerEvents: 'none'
+            });
+        } else {
+            $('#custom-cursor').css({
+                left:  e.pageX,
+                top:   e.pageY,
+                "background-image": "url('img/cursor-normal.png')",
+                pointerEvents: 'none'
+            });
+        }
+    
+    });
+
+})
+
+
