951e926243cb7a92b30f67eb3f946160fbbb81e7
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-10-02 22:07:47 -0400

    added tv display

--- public/scrolly.js
+++ public/scrolly.js
@@ -1,23 +1,75 @@
-const canvas = document.getElementById("canvas-scroll");
-const ctx = canvas.getContext("2d");
-const middleX = canvas.width/2;
-const middleY = canvas.height/2;
-
-
 $(document).ready(function() {
     'use strict';
+    const canvas = document.getElementById("canvas-scroll");
+    const ctx = canvas.getContext("2d");
+    const WIDTH = canvas.width-130;
+    const X0 = 135;
+    const HEIGHT = canvas.height-150;
+    const Y0 = 50;
+    const middleX = (WIDTH-X0)/2 + X0;
+    const middleY = (HEIGHT-Y0)/2 + Y0;
+    const spriteSheet = document.getElementById("spritesheet");
+    const frameWidth = 16; // Width of a single frame in pixels
+    const frameHeight = 16; // Height of a single frame in pixels
+    const frameCount = 2; // Number of frames in the spritesheet
+        // sprite sheet animation
+    let timeFrame = 0;
+    let currentFrame = 0; // The current frame to display (on row)
+    let frameRow = 0;
     paper.install(window);
-    paper.setup(canvas);
-    var tool = new Tool();
 
+    const player = {
+        x: middleX,
+        y: HEIGHT - 54,
+        width: 54,
+        height: 54,
+        speedY: 8,
+        defaultSpeed: 8
+    };
 
-    tool.onMouseDown = function(event) {
-        var c = Shape.Circle(event.point.x, event.point.y, 50);
-        c.fillColor = 'green';
+    function updatePlayerFrame() {
+        if (timeFrame <= 10) {
+            timeFrame++;
+        } else {
+            currentFrame = (currentFrame + 1) % frameCount;
+            timeFrame = 0;
+        }
     }
 
+    function playFrames() {
+        updatePlayerFrame();
+        paper.setup(canvas);
+        var tool = new Tool();
+    
+    
+        var tv = new Raster('tv');
+        tv.position = view.center;
+        tv.scale(canvas.height/tv.height * 0.83);
+    
+        var c = Shape.Circle(middleX, middleY, 5);
+        c.fillColor = 'green';
 
-    paper.view.draw();
+
+    
+
+
+        paper.view.draw();
+        ctx.drawImage(
+            spriteSheet,
+            currentFrame * frameWidth + 2,
+            frameRow * frameHeight,
+            frameWidth,
+            frameHeight,
+            player.x,
+            player.y,
+            player.width,
+            player.height
+        );
+        requestAnimationFrame(playFrames); // continue the animation loop
+
+    }
+
+    // start the animation loop
+    requestAnimationFrame(playFrames);
 })
 
-console.log("scrolly js  loaded");
--- public/slides.js
+++ public/slides.js
@@ -1,4 +1,38 @@
-console.log("slides js loaded");
-const slides = document.getElementsByTagName("template");
+// console.log("slides js loaded");
+// const slideNum = 8;
 
-slides[0].style.display = "inline-block";
+// const slideWrap = document.getElementById("horizantal-slide-extend");
+// let translatPercent = 0;
+// var currentSlideIndex = 0;
+// const prev = document.getElementById("prev");
+// const next = document.getElementById("next");
+
+
+
+
+// document.addEventListener('keydown', (event))
+
+
+
+
+
+
+// function generateSlides(slideNum) {
+//     for (i = 0; i < slideNum; i++) {
+//         const slideDiv = document.createElement('div');
+//         slideDiv.className = 'slide'; // Set the class name
+//         const leftAmount = 100 + 90*i;
+//         slideDiv.style.left = leftAmount+"%";
+
+//         // Create an h1 element and set its content
+//         const h1Element = document.createElement('h1');
+//         h1Element.textContent = `Slide ${i + 1}`; // Content of the h1
+
+//         // Append the h1 element to the slideDiv
+//         slideDiv.appendChild(h1Element);
+//         slideWrap.appendChild(slideDiv);
+
+//     }
+// }
+
+// generateSlides(slideNum);
